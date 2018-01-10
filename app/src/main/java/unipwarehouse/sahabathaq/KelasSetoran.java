552523f7cb6536920814162a237ec1d7a8957d15
Abdurrahman Al Hanif <abdurrahman.alhanif@gmail.com>
2018-01-10 13:14:16 +0700

    initial commit

---
+++ app/src/main/java/unipwarehouse/sahabathaq/KelasSetoran.java
@@ -0,0 +1,29 @@
+package unipwarehouse.sahabathaq;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class KelasSetoran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelas_setoran);
+
+        // Title fontface
+        TextView txt = (TextView) findViewById(R.id.namaKelas);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/cinzel-bold.ttf");
+        txt.setTypeface(font);
+    }
+
+    //Menu options kelas setoran
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.kelas_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
